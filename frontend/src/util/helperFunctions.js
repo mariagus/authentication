4c7aad7807c90d790918d87d51023b1a8f5f7f7d
Maria Gusova <masha.gu@gmail.com>
2021-01-26 20:40:53 +0000

    refactor function name

--- frontend/src/util/helperFunctions.js
+++ frontend/src/util/helperFunctions.js
@@ -1,4 +1,4 @@
 export default function jwtDecoder(token) {
-  const parts = token.split(".");
-  return JSON.parse(atob(parts[1]));
+  const arr = token.split(".");
+  return JSON.parse(atob(arr[1]));
 }
