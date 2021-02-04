a8aed7f86fc14554087188fd85631a1febfb97dd
Maria Gusova <masha.gu@gmail.com>
2021-02-04 12:41:27 +0000

    remove unwanted get route

--- routes/users.js
+++ routes/users.js
@@ -90,10 +90,4 @@ router.get(
   }
 );
 
-router.route("/").get((req, res) => {
-  User.find()
-    .then((User) => res.json(User))
-    .catch((err) => res.status(400).json("Error: " + err));
-});
-
 module.exports = router;
